a989ca0266fc1a8acb4dd0e1797dd670aff55dd7
Diogo Santos <diogodeese@gmail.com>
2022-05-27 23:48:53 +0100

    Added Kick Command

---
+++ commands/mod/kick.js
@@ -0,0 +1,52 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { Permissions } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("kick")
+    .setDescription("Kicks a user from the server")
+    .addUserOption((option) =>
+      option
+        .setName("user")
+        .setDescription("The user to kick")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const user = interaction.options.getUser("user");
+    const member = interaction.member;
+    const guild = interaction.guild;
+
+    if (!member.permissions.has([Permissions.FLAGS.KICK_MEMBERS])) {
+      interaction.reply("You don't have permission to kick members.");
+      return;
+    }
+
+    if (user.id === guild.ownerID) {
+      interaction.reply("You can't kick the owner of the server.");
+      return;
+    }
+
+    if (user.id === interaction.client.user.id) {
+      interaction.reply("You can't kick me.");
+      return;
+    }
+
+    if (user.id === member.id) {
+      interaction.reply("You can't kick yourself.");
+      return;
+    }
+
+    if (!user.kickable) {
+      interaction.reply("I can't kick this user.");
+      return;
+    }
+
+    try {
+      interaction.options.getMember("user").kick();
+      interaction.reply(`Kicked ${user.tag}`);
+    } catch (err) {
+      interaction.reply(`Something went wrong when kicking ${user.tag}`);
+      console.error(err);
+    }
+  },
+};
