e3abe245696925c217dc5a23d201fb5d9211cc0e
Diogo Santos <diogodeese@gmail.com>
2022-09-23 20:12:28 +0100

    style: change positions

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -17,28 +17,29 @@ module.exports = {
       }`;
     });
 
-    return interaction.reply({
-      embeds: [
-        {
-          title: "Music Queue",
-          description: `${tracks.join("\n")}${
-            queue.tracks.length > tracks.length
-              ? `\n\n${
-                  queue.tracks.length - tracks.length === 1
-                    ? `${queue.tracks.length - tracks.length} more track`
-                    : `${queue.tracks.length - tracks.length} more tracks`
-                }`
-              : ""
-          }`,
-          color: "#0099ff",
-          fields: [
-            {
-              name: "Now Playing",
-              value: `[**${currentTrack.title}**](${currentTrack.url}) - ${currentTrack.requestedBy.tag}`,
-            },
-          ],
-        },
-      ],
-    });
+    const embed = new MessageEmbed()
+      .setColor("#0099ff")
+      .setTitle("Music Queue")
+      .addFields({
+        name: "Now Playing",
+        value: `[**${currentTrack.title}**](${currentTrack.url}) - ${currentTrack.requestedBy.tag}`,
+        inline: true,
+      })
+      .setDescription(
+        `${tracks.join("\n")}${
+          queue.tracks.length > tracks.length &&
+          `\n\n${
+            queue.tracks.length - tracks.length === 1
+              ? `${queue.tracks.length - tracks.length} more track`
+              : `${queue.tracks.length - tracks.length} more tracks`
+          }`
+        }`
+      )
+      .setAuthor(
+        "Evee",
+        "https://cdn.discordapp.com/avatars/775530325572976640/67386d9c99041abd20a890018ac2b497.png"
+      );
+
+    return interaction.Reply({ content: "", embeds: [embed] });
   },
 };
